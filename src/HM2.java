dcaf26016079f9aa2f8f697054e81e8339fc4c72
AsuyLas <asa1216911@gmail.com>
2023-11-19 05:42:38 +0200

    Домашнее задание 2

---
+++ src/HM2.java
@@ -0,0 +1,15 @@
+public class HM2 {
+    public static void calculateAndPrint() {
+        int number = 123;
+        int num1 = 1;
+        int num2 = 2;
+        int num3 = 3;
+        int sum = num1 + num2 + num3; // Сумма цифр
+
+        System.out.println("Сумма цифр числа " + number + " равна " + sum);
+    }
+
+    public static void main(String[] args) {
+        calculateAndPrint(); // Вызываем метод для вычисления и печати суммы цифр числа 123
+    }
+}
