d5fa218bf3973c988d4b90606013071fbd46275b
AsuyLas <asa1216911@gmail.com>
2023-11-19 02:29:10 +0200

    Initial commit

---
+++ src/HM1.java
@@ -0,0 +1,14 @@
+public class HM1 {
+    static class myClass {
+        int Year = 2023;
+
+        public static int MYMETHOD(int a, int b) {
+            int c = a + b;
+            return c;
+        }
+
+        public static void main(String[] args) {
+            MYMETHOD(4, 6);
+        }
+    }
+}
