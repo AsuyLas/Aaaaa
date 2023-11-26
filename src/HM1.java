6977a52a6ae6df84e48c8f7dc13d97486aca1603
AsuyLas <asa1216911@gmail.com>
2023-11-26 13:15:11 +0200

    Домашнее задание

--- src/HM1.java
+++ src/HM1.java
@@ -1,15 +1,14 @@
 public class HM1 {
-    static class myClass {
-        int Year = 2023;
+    int year = 2023;
 
-        public static int MYMETHOD(int a, int b) {
-            int c = a + b;
-            return c;
-        }
-
-        public static void main(String[] args) {
-            int result = MYMETHOD(4, 6);
-            System.out.println("Результат: " + result);
-        }
+    public static int sumOfTwoNumbers(int a, int b) {
+        int c = a + b;
+        return c;
     }
+
+    public static void main(String[] args) {
+        int result = sumOfTwoNumbers(4, 6);
+        System.out.println("Result: " + result);
+    }
+
 }
