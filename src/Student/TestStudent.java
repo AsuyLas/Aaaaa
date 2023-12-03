4de3ae124d5866b67de81a8045c408ec5296fb12
AsuyLas <asa1216911@gmail.com>
2023-12-03 13:35:08 +0200

    Homework five

--- src/Student/TestStudent.java
+++ src/Student/TestStudent.java
@@ -2,7 +2,6 @@ package Student;
 
 public class TestStudent {
     public static void main(String[] args) {
-        // Створення об'єкту класу Student і виклик методу showInfo
         Student student = new Student("John", 2, 8);
         student.showInfo();
     }
