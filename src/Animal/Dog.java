2684feee576a89761e770b20e13aaeb87fdd93bc
AsuyLas <asa1216911@gmail.com>
2023-12-03 13:50:15 +0200

    Homework

---
+++ src/Animal/Dog.java
@@ -0,0 +1,11 @@
+package Animal;
+
+class Dog extends Pet {
+    Dog(String name) {
+        System.out.println("I am dog and my name is: " + name);
+    }
+
+    void play() {
+        System.out.println("Dog plays");
+    }
+}
---
+++ src/Student/TestStudent.java
@@ -0,0 +1,8 @@
+package Student;
+
+public class TestStudent {
+    public static void main(String[] args) {
+        Student student = new Student("John", 2, 8);
+        student.showInfo();
+    }
+}
